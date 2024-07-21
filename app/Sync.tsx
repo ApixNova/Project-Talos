5ec262b28056fed3aeb2eeb047666fb50f498f1a
Apix Nova <laangryanas@gmail.com>
2024-07-21 03:13:12 +0200

    feat: Auth component styled

--- app/Sync.tsx
+++ app/Sync.tsx
@@ -1,4 +1,10 @@
-import { Pressable, View, Text, StyleSheet } from "react-native";
+import {
+  Pressable,
+  View,
+  Text,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
 import Auth from "../components/Auth";
 import { palette } from "../utils/palette";
 import { useEffect, useState } from "react";
@@ -8,6 +14,7 @@ import { syncDatabase } from "../utils/sync";
 
 export default function Screen() {
   const [session, setSession] = useState<Session | null>(null);
+  const { height } = useWindowDimensions();
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -18,26 +25,43 @@ export default function Screen() {
     });
   }, []);
 
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+  }
+
   return (
     <View style={styles.container}>
-      <Auth />
-      <Pressable
-        onPress={() => {
-          syncDatabase();
-        }}
-        style={{
-          backgroundColor: "pink",
-          padding: 5,
-          borderRadius: 10,
-        }}
-      >
-        <Text>Sync</Text>
-      </Pressable>
-      {session && session.user && (
-        <>
+      {session && session.user ? (
+        <View style={styles.logged}>
           <Text style={styles.text}>User Connected!</Text>
           <Text style={styles.text}>{session.user.email}</Text>
-        </>
+          <Pressable style={styles.button} onPress={signOut}>
+            <Text style={styles.text}>Log out</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              syncDatabase();
+            }}
+            style={{
+              backgroundColor: "pink",
+              padding: 5,
+              borderRadius: 10,
+            }}
+          >
+            <Text>Sync</Text>
+          </Pressable>
+        </View>
+      ) : (
+        <View
+          style={[
+            styles.auth,
+            {
+              marginTop: height / 5,
+            },
+          ]}
+        >
+          <Auth />
+        </View>
       )}
     </View>
   );
@@ -47,11 +71,22 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: palette.background,
     height: "100%",
-    justifyContent: "space-around",
+    // justifyContent: "space-around",
     alignItems: "center",
   },
   text: {
     color: "white",
     fontFamily: "Inter_400Regular",
   },
+  logged: {
+    width: "50%",
+    height: "50%",
+  },
+  auth: {
+    // marginTop: "10%",
+    height: "50%",
+    width: "100%",
+    // backgroundColor: "pink",
+  },
+  button: { backgroundColor: palette.rose, padding: 2 },
 });
--- components/Auth.tsx
+++ components/Auth.tsx
@@ -8,8 +8,6 @@ import {
   StyleSheet,
 } from "react-native";
 import { supabase } from "../utils/supabase";
-import { FontAwesome6 } from "@expo/vector-icons";
-import { Foundation } from "@expo/vector-icons";
 import { palette } from "../utils/palette";
 
 export default function Auth() {
@@ -26,6 +24,8 @@ export default function Auth() {
 
     if (error) Alert.alert(error.message);
     setLoading(false);
+
+    setPassword("");
   }
 
   async function signUpWithEmail() {
@@ -42,51 +42,64 @@ export default function Auth() {
     if (!session)
       Alert.alert("Please check your inbox for email verification!");
     setLoading(false);
+
+    setPassword("");
   }
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.text}>Email</Text>
-        <View style={styles.inputContainer}>
-          <Foundation name="mail" size={37} color="white" />
+      <Text style={styles.title}>Sign in</Text>
+      <View style={styles.inputContainer}>
+        <View>
+          <Text style={styles.text}>Email</Text>
           <TextInput
             style={styles.input}
             autoComplete="email"
             inputMode="email"
             onChangeText={setEmail}
             value={email}
-            placeholder="email@address.com"
             placeholderTextColor={"gray"}
           />
         </View>
-      </View>
-      <View>
-        <Text style={styles.text}>Password</Text>
-        <View style={styles.inputContainer}>
-          <FontAwesome6 name="lock" size={37} color="white" />
+        <View>
+          <Text style={styles.text}>Password</Text>
           <TextInput
             style={styles.input}
             autoComplete="password"
             secureTextEntry={true}
             onChangeText={setPassword}
             value={password}
-            placeholder="Password"
             placeholderTextColor={"gray"}
           />
         </View>
-      </View>
-      <View style={styles.buttonContainer}>
-        {!loading && (
-          <Pressable style={styles.button} onPress={signInWithEmail}>
-            <Text style={styles.buttonText}>Sign in</Text>
-          </Pressable>
-        )}
-        {!loading && (
-          <Pressable style={styles.button} onPress={signUpWithEmail}>
-            <Text style={styles.buttonText}>Sign up</Text>
-          </Pressable>
-        )}
+        <View style={styles.buttonContainer}>
+          {!loading && (
+            <Pressable
+              style={[styles.button, styles.signIn]}
+              onPress={signInWithEmail}
+            >
+              <Text style={styles.buttonText}>Sign In</Text>
+            </Pressable>
+          )}
+          {!loading && (
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                margin: "auto",
+              }}
+            >
+              <Text style={styles.signUpText}>New account ?</Text>
+              <Pressable
+                style={[styles.button, styles.signUp]}
+                onPress={signUpWithEmail}
+              >
+                <Text style={styles.buttonText}>Sign Up</Text>
+              </Pressable>
+            </View>
+          )}
+        </View>
       </View>
     </View>
   );
@@ -94,48 +107,62 @@ export default function Auth() {
 
 const styles = StyleSheet.create({
   container: {
-    maxWidth: 900,
-    borderWidth: 2,
-    borderColor: "pink",
+    maxWidth: 600,
     borderRadius: 7,
     width: "90%",
-    // margin: "auto",
+    backgroundColor: palette.accent,
+    marginHorizontal: "auto",
     padding: 5,
+  },
+  title: {
+    fontSize: 25,
+    color: palette.background,
+    fontFamily: "Inter_500Medium",
+    marginBottom: 30,
+    marginTop: 10,
+    marginHorizontal: "auto",
   },
   text: {
     fontSize: 20,
-    color: palette.text,
-    marginHorizontal: "auto",
-    fontFamily: "Inter_400Regular",
+    fontFamily: "Inter_500Medium",
+    color: palette.background,
   },
   input: {
-    color: palette.text,
     borderWidth: 2,
+    borderRadius: 9,
+    fontSize: 17,
     borderColor: "pink",
-    width: "90%",
     fontFamily: "Inter_400Regular",
-    padding: 2,
-    margin: 2,
+    padding: 5,
   },
   inputContainer: {
-    margin: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    // gap: 10,
+    width: "90%",
+    gap: 10,
+    marginHorizontal: "auto",
+    marginBottom: 20,
   },
   buttonContainer: {
-    flexDirection: "row",
+    gap: 8,
   },
   button: {
     padding: 5,
-    borderWidth: 2,
-    borderColor: "pink",
     borderRadius: 7,
-    margin: "auto",
+  },
+  signIn: {
+    backgroundColor: palette.primary,
+    paddingVertical: 9,
+  },
+  signUp: {
+    backgroundColor: palette.rose,
+  },
+  signUpText: {
+    fontSize: 17,
+    fontFamily: "Inter_500Medium",
   },
   buttonText: {
-    color: "white",
+    color: palette.text,
     fontFamily: "Inter_400Regular",
     fontSize: 17,
+    margin: "auto",
   },
 });
